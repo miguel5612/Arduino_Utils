072722da67638caf275e48a220cc5ec6fa502209
miguel5612 <miguelangelcu@ufps.edu.co>
2018-09-19 20:10:20 -0500

    Se modifica el ejemplo para usar la lcd i2c

--- Micro - 2018/Clase 3/Ejercicios/Ejercicios.ino
+++ Micro - 2018/Clase 3/Ejercicios/Ejercicios.ino
@@ -1,23 +1,28 @@
-#include<LiquidCrystal.h> //Library for LCD
+//#include<LiquidCrystal.h> //Library for LCD
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
 
-#define btnUp 7
-#define btnSet 6 
-#define LED1 8
-#define LED2 9
-#define LED3 10
+#define btnUp 10
+#define btnSet 11 
+#define LED1 12
+#define LED2 13
+#define LED3 9
 #define timeDelay 200
 
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);// LCD pins at which it is attached to the Arduino
+//LiquidCrystal lcd(12, 11, 5, 4, 3, 2);// LCD pins at which it is attached to the Arduino
+LiquidCrystal_I2C lcd(0x3F,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
-bool bandera = false;
+bool bandera = false, bandera2 = false;
 String movStr = "";
 int menuEx = 1;
 int menu = 1;
  
 void setup() //method used to run the source for the one time onlys
 {
+    lcd.init();
+    lcd.backlight();
     Serial.begin(9600);    
-    lcd.begin(16, 2);//LCD order i.e. 16 columns & 2 rows
+    //lcd.begin(16, 2);//LCD order i.e. 16 columns & 2 rows
   
     pinMode(LED1,OUTPUT);
     pinMode(LED2,OUTPUT);
@@ -36,6 +41,7 @@ void loop() //method to run the source code repeatedly
       break;
     case 2:
       ejemplo2();
+      bandera2 = true;
       break;
     case 3:
       ejemplo3();
@@ -81,37 +87,39 @@ void ejemplo1(){
   }
 }
 void ejemplo2(){
-  switch(menuEx){
-        case 1:
-          lcd.print("Target: LED_1");
-          lcd.setCursor(0,1);
-          lcd.print("Estado=");
-          lcd.print(digitalRead(LED1));
-          delay(timeDelay);
-          break;
-        case 2:
-          lcd.print("Target: LED_2");
-          lcd.setCursor(0,1);
-          lcd.print("Estado=");
-          lcd.print(digitalRead(LED2));
-          delay(timeDelay);
-          break;
-         case 3:
-           lcd.print("Target: LED_3");
-          lcd.setCursor(0,1);
-          lcd.print("Estado=");
-          lcd.print(digitalRead(LED3));
-           delay(timeDelay);
-           break;
-  }
+  if(bandera2){   
   lcd.setCursor(0,0);//setting cursor on LCD
   lcd.clear();
+  switch(menuEx){
+          case 1:
+            lcd.print("Target: LED_1");
+            lcd.setCursor(0,1);
+            lcd.print("Estado=");
+            lcd.print(digitalRead(LED1));
+            break;
+          case 2:
+            lcd.print("Target: LED_2");
+            lcd.setCursor(0,1);
+            lcd.print("Estado=");
+            lcd.print(digitalRead(LED2));
+            break;
+           case 3:
+             lcd.print("Target: LED_3");
+            lcd.setCursor(0,1);
+            lcd.print("Estado=");
+            lcd.print(digitalRead(LED3));
+             break;
+    }
+    bandera2 = false;
+  }
     if(!digitalRead(btnUp)) {
-      if(menuEx>3){
-        menuEx = 0;
+      bandera2 = true;
+      if(menuEx>2){
+        menuEx = 1;
       }else{
         menuEx++;
       }
+      delay(timeDelay*2);
     }else if(!digitalRead(btnSet)){
         switch(menuEx){
       case 1:
@@ -123,7 +131,9 @@ void ejemplo2(){
       case 3:
         digitalWrite(LED3,!digitalRead(LED3));
         break;
-    } 
+    }
+    bandera2 = true;
+    delay(timeDelay*2); 
     }
     
 }
